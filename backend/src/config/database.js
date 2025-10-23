732addd1722ec9b494fc6be2bb7359d70b78ddb5
ChathurK <kumaras-im21001@stu.kln.ac.lk>
2025-10-23 12:48:51 +0530

    added final database script

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,7 +8,7 @@ dotenv.config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '1234',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'photo',
   port: parseInt(process.env.DB_PORT) || 3307,
   waitForConnections: true,
